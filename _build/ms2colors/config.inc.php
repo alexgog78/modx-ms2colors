f9a511e725263c828b95ad3a3817e1468349f06f
Aleksey <a.goguev@alexgog.ru>
2020-03-04 22:07:42 +0300

    Version fix, new package

--- _build/ms2colors/config.inc.php
+++ _build/ms2colors/config.inc.php
@@ -2,5 +2,5 @@
 
 define('PKG_NAME', 'ms2Colors');
 define('PKG_NAME_LOWER', 'ms2colors');
-define('PKG_VERSION', '1.0');
-define('PKG_RELEASE', 'rc1');
+define('PKG_VERSION', '1.0.0');
+define('PKG_RELEASE', 'beta');
